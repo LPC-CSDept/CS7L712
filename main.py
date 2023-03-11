0b5ede9caf80463565c0f4aa6d3d2f0ac9acf327
Kyuwoong Lee <kwooler@gmail.com>
2023-03-11 14:43:50 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+rsum = []
+
+
+def main():
+    numbers = [[11, 12, 13, 14, 15],
+               [21, 22, 23, 24, 25],
+               [31, 32, 33, 34, 35]]
+    global rsum     # Use this global variable to save the summation of the row values
+    ##################################################
+    # Comlete your code here
+    ##################################################
+
+
+if __name__ == '__main__':
+    main()
---
+++ main_test.py
@@ -0,0 +1,32 @@
+import main
+import io
+import sys
+import re
+
+# global rsum
+
+
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = '2'
+    # sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # regex_string = r'[\w,\W]*165[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
+    # assert main.main.rsum[0] == 65
+    # assert main.main.rsum[1] == 115
+    # assert main.main.rsum[2] == 165
+    assert main.rsum[0] == 65
+    assert main.rsum[1] == 115
+    assert main.rsum[2] == 165
