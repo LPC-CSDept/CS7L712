fc14ff79ced65253b3d1038cea9db8e13c57e046
Kyu Lee <kwooler@gmail.com>
2023-08-10 18:26:35 -0700

    712

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-csum = []
+def getSumCol(numbers):
+    """
+    ################################################## 
+    Code your program here
+    ################################################## 
+    """
+    return result
 
 
 def main():
     numbers = [[11, 12, 13, 14, 15],
                [21, 22, 23, 24, 25],
                [31, 32, 33, 34, 35]]
-    global csum     # Use this global variable to save the summation of the column values
-    ##################################################
-    # Comlete your code here
-    ##################################################
+
+    result = getSumCol(numbers)
+    print(result)
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -7,28 +7,34 @@ import re
 
 
 def test_main_1():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = '2'
-    # sys.stdin = io.StringIO(datastr)
+    numbers = [[11, 12, 13, 14, 15],
+               [21, 22, 23, 24, 25],
+               [31, 32, 33, 34, 35]]
 
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
+    result = main.getSumCol(numbers)
+    print('Your output ', result)
+    assert len(result) == 5
+    assert result[0] == 63
+    assert result[1] == 66
+    assert result[2] == 69
+    assert result[3] == 72
+    assert result[4] == 75
 
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    # regex_string = r'[\w,\W]*165[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    # assert res != None
-    # print(res.group())
-    # assert main.main.rsum[0] == 65
-    # assert main.main.rsum[1] == 115
-    # assert main.main.rsum[2] == 165
-    assert main.csum[0] == 63
-    assert main.csum[1] == 66
-    assert main.csum[2] == 69
-    assert main.csum[3] == 72
-    assert main.csum[4] == 75
+
+def test_main_2():
+    import random
+    rownum = random.randint(3, 6)
+    colnum = random.randint(3, 7)
+
+    numbers = [[0] * colnum for i in range(rownum)]
+    for i in range(rownum):
+        for j in range(colnum):
+            numbers[i][j] = random.randint(0, 10)
+        print(numbers[i])
+    result = main.getSumCol(numbers)
+    print('Your output', result)
+
+    assert len(result) == colnum
+    translist = list(zip(*numbers))
+    for i in range(len(translist)):
+        assert result[i] == sum(translist[i])
